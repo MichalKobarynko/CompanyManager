785e2f6bd4d511f34df367ecc6da544cb643f979
MichalKobarynko <kobarynkomichal@gmail.com>
2023-10-03 00:10:11 +0200

    NA dziś ma dosyć :)

--- CompanyManager/ClientApp/src/app/border/accordion/accordion.component.ts
+++ CompanyManager/ClientApp/src/app/border/accordion/accordion.component.ts
@@ -21,8 +21,32 @@ export class AccordionComponent {
         id: '1',
         title: 'Projekt A',
         users: ['user1', 'user2', 'user3'],
-        userId: 'user1',
+        userId: '12c75593-2034-4824-81b5-4f4d40d47311',
         boards: [
+          {
+            id: '1',
+            name: 'boardxxxxxxx xxxxxsssseeeeeeeeeeeeeeeeeee',
+            columns: [],
+            projectId: '1',
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            id: '2',
+            name: 'board1',
+            columns: [],
+            projectId: '1',
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            id: '3',
+            name: 'board3',
+            columns: [],
+            projectId: '1',
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
         ],
         createdAt: new Date('2023-01-15'),
         updatedAt: new Date('2023-02-20'),
@@ -31,7 +55,7 @@ export class AccordionComponent {
         id: '2',
         title: 'Projekt B',
         users: ['user2', 'user4'],
-        userId: 'user2',
+        userId: '12c75593-2034-4824-81b5-4f4d40d47311',
         boards: [
         ],
         createdAt: new Date('2023-02-10'),
@@ -41,7 +65,7 @@ export class AccordionComponent {
         id: '3',
         title: 'Projekt C',
         users: ['user1', 'user3', 'user5'],
-        userId: 'user3',
+        userId: '12c75593-2034-4824-81b5-4f4d40d47311',
         boards: [
           
         ],
