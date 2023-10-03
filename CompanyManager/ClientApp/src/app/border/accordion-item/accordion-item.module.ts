63a1fd6a7966aceba704136574cd9cc6b9afb0c9
MichalKobarynko <kobarynkomichal@gmail.com>
2023-10-03 21:50:14 +0200

    Dodanie context-menu

--- CompanyManager/ClientApp/src/app/border/accordion-item/accordion-item.module.ts
+++ CompanyManager/ClientApp/src/app/border/accordion-item/accordion-item.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { AccordionItemComponent } from "./accordion-item.component";
 import { CollapseButtonModule } from "../collapse-button/collapse-button.module";
 import { CommonModule } from "@angular/common";
+import { ContextMenuModule } from "../context-menu/context-menu.module";
 
 @NgModule({
   declarations: [AccordionItemComponent],
-  imports: [CommonModule, CollapseButtonModule],
+  imports: [CommonModule, CollapseButtonModule, ContextMenuModule],
   exports: [AccordionItemComponent],
 })
 export class AccordionItemModule { }
